b64e49e712f3345da79d1730b916ce07226b4f45
Aman Agarwal <aman.a.agg@gmail.com>
2018-09-09 12:58:51 +0930

    Commit 5
    Feeds page

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ function myMap() {
 }
 
 
-function changeScreen(param) {
+function setScreen(param) {
   $('#1').css({'display': 'none'});
   $('#2').css({'display': 'none'});
   $('#3').css({'display': 'none'});
