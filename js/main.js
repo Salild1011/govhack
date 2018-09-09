8064c7d524c4e6f79b115076236127787a85a59e
Aman Agarwal <aman.a.agg@gmail.com>
2018-09-09 12:37:06 +0930

    Commit 4
    Added page functionality

--- js/main.js
+++ js/main.js
@@ -1,88 +1,22 @@
 function myMap() {
-    var mapOptions = {
-        center: new google.maps.LatLng(-34.922851, 138.602320),
-        mapTypeId: google.maps.MapTypeId.HYBRID,
-        zoom: 12,
-        styles: [
-          {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-          {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-          {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-          {
-            featureType: 'administrative.locality',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#d59563'}]
-          },
-          {
-            featureType: 'poi',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#d59563'}]
-          },
-          {
-            featureType: 'poi.park',
-            elementType: 'geometry',
-            stylers: [{color: '#263c3f'}]
-          },
-          {
-            featureType: 'poi.park',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#6b9a76'}]
-          },
-          {
-            featureType: 'road',
-            elementType: 'geometry',
-            stylers: [{color: '#38414e'}]
-          },
-          {
-            featureType: 'road',
-            elementType: 'geometry.stroke',
-            stylers: [{color: '#212a37'}]
-          },
-          {
-            featureType: 'road',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#9ca5b3'}]
-          },
-          {
-            featureType: 'road.highway',
-            elementType: 'geometry',
-            stylers: [{color: '#746855'}]
-          },
-          {
-            featureType: 'road.highway',
-            elementType: 'geometry.stroke',
-            stylers: [{color: '#1f2835'}]
-          },
-          {
-            featureType: 'road.highway',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#f3d19c'}]
-          },
-          {
-            featureType: 'transit',
-            elementType: 'geometry',
-            stylers: [{color: '#2f3948'}]
-          },
-          {
-            featureType: 'transit.station',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#d59563'}]
-          },
-          {
-            featureType: 'water',
-            elementType: 'geometry',
-            stylers: [{color: '#17263c'}]
-          },
-          {
-            featureType: 'water',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#515c6d'}]
-          },
-          {
-            featureType: 'water',
-            elementType: 'labels.text.stroke',
-            stylers: [{color: '#17263c'}]
-          }
-        ]
-    }
-	var map = new google.maps.Map(document.getElementById("map123"), mapOptions);
+  var mapOptions = {
+      center: new google.maps.LatLng(-34.922851, 138.602320),
+      zoom: 12,
+      mapTypeId: google.maps.MapTypeId.MAP
+  }
+  var map = new google.maps.Map(document.getElementById("map1"), mapOptions);
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(-34.8604311, 138.5177885),
+    title:"Hello World!"
+  });
+  marker.setMap(map);
+}
+
+
+function changeScreen(param) {
+  $('#1').css({'display': 'none'});
+  $('#2').css({'display': 'none'});
+  $('#3').css({'display': 'none'});
+
+  $('#'+param).css({'display': 'block'});  
 }
